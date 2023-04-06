c6f2374e3d64d5655e14b92841d62e4aa9181f9e
Muhammad <Muhammad.medhat@gmail.com>
2023-04-06 22:13:33 +0200

    setup gh pages

---
+++ src/components/Banner.js
@@ -0,0 +1,115 @@
+import { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import headerImg from "../assets/img/header-img.svg";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import "animate.css";
+import TrackVisibility from "react-on-screen";
+
+export const Banner = () => {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [text, setText] = useState("");
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const [index, setIndex] = useState(1);
+  const toRotate = ["Web Developer", "Web Designer", "UI/UX Designer"];
+  const period = 2000;
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => {
+      clearInterval(ticker);
+    };
+  }, [text]);
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta((prevDelta) => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setIndex((prevIndex) => prevIndex - 1);
+      setDelta(period);
+    } else if (isDeleting && updatedText === "") {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setIndex(1);
+      setDelta(500);
+    } else {
+      setIndex((prevIndex) => prevIndex + 1);
+    }
+  };
+
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="aligh-items-center">
+          <Col xs={12} md={6} xl={7}>
+            <TrackVisibility>
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__fadeIn" : ""
+                  }
+                >
+                  <span className="tagline">Welcome to my Portfolio</span>
+                  <h1>
+                    {`Hi! I'm Muhammad  Medhat`}{" "}
+                    <span
+                      className="txt-rotate"
+                      dataPeriod="1000"
+                      data-rotate='[ "JavaScript Web Developer", "Wordpress Developer" ]'
+                    >
+                      <span className="wrap">{text}</span>
+                    </span>
+                  </h1>
+                  <p>
+                    Hi there! I am a web developer with expertise in JavaScript,
+                    React, WordPress, and PHP. I have a passion for creating
+                    beautiful and functional websites that provide a seamless
+                    user experience. With several years of experience under my
+                    belt, I have developed a deep understanding of web
+                    development best practices and stay up-to-date with the
+                    latest technologies and trends. Whether it's building a
+                    custom WordPress site or creating a dynamic web application
+                    with React, I am dedicated to delivering high-quality work
+                    that exceeds expectations. When I'm not coding, you can find
+                    me exploring new hiking trails or catching up on the latest
+                    tech news.
+                  </p>
+                  <button onClick={() => console.log("connect")}>
+                    Let’s Connect <ArrowRightCircle size={25} />
+                  </button>
+                </div>
+              )}
+            </TrackVisibility>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <TrackVisibility>
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__zoomIn" : ""
+                  }
+                >
+                  <img src={headerImg} alt="Header Img" />
+                </div>
+              )}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
---
+++ src/components/ProjectCard.js
@@ -0,0 +1,23 @@
+import { Col } from "react-bootstrap";
+
+export const ProjectCard = ({ title, desc, image }) => {
+  return (
+    <Col size={12} sm={6} md={4}>
+      <div
+        className="proj-imgbx"
+        // style={{
+        //   backgroundImage: `url(${image})`,
+        //   backgroundSize: "cover",
+        //   border: "solid",
+        //   height: "400px",
+        // }}
+      >
+        <img src={image} />
+        <div className="proj-txtx">
+          <h4>{title}</h4>
+          <span>{desc}</span>
+        </div>
+      </div>
+    </Col>
+  );
+};
