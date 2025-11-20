f52ed23c01c5cbba10a8017dad83566dac4846df
muhammad medhat <muhammad.medhat@gmail.com>
2025-11-20 23:01:05 +0200

    fix:gitignore

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import "./styles/utility-classes.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="portfolio">
+    <BrowserRouter basename="/portfolio">
       <App />
     </BrowserRouter>
   </React.StrictMode>
