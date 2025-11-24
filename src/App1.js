8d7fc261020b42c8973c400345e93b71af058d3a
muhammad medhat <muhammad.medhat@gmail.com>
2025-11-24 13:40:00 +0200

    feat: implement main application structure with routing and loading screen

---
+++ src/App1.js
@@ -0,0 +1,169 @@
+import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.css";
+import { Route, Routes } from "react-router-dom";
+import { Footer } from "./com/footer";
+import "./assets/sass/main-md.scss";
+import Sidebar from "./com/Sidebar";
+
+//lazy load pages
+// const Home = React.lazy(() => import("./pages/Home"));
+// const About = React.lazy(() => import("./pages/about/"));
+// const Contact = React.lazy(() => import("./pages/contact"));
+// const RSSApp = React.lazy(() => import("./com/RssApp"));
+// const YoutubeChannelVideos = React.lazy(() =>
+//   import("./com/youtube/YoutubeChannelVideos")
+// );
+//import without lazy load
+import Home from "./pages/Home";
+import About from "./pages/about/";
+import Contact from "./pages/contact";
+import RSSApp from "./com/RssApp";
+import YoutubeChannelVideos from "./com/youtube/YoutubeChannelVideos";
+import Loader from "./com/loader/loader";
+
+function App() {
+  document.title = "Muhammad Medhat - Wordpress Developer";
+  const [loading, setLoading] = useState(true);
+  const scrollTop = () => {
+    const contentDiv = document.getElementById("main");
+    if (contentDiv) {
+      contentDiv.scrollIntoView({ behavior: "smooth", block: "start" });
+    }
+  };
+  useEffect(() => {
+    // Wait for ALL images + fonts + assets to load
+    const handleLoad = () => {
+      setLoading(false);
+    };
+
+    if (document.readyState === "complete") {
+      handleLoad();
+    } else {
+      window.addEventListener("load", handleLoad);
+    }
+
+    return () => window.removeEventListener("load", handleLoad);
+  }, []);
+
+  if (loading) {
+    return <Loader />; // show loading screen
+  }
+  return (
+    <div className="wrapper">
+      <header id="header" className="flex flex-column justify-content-around">
+        <Sidebar />
+      </header>
+      <main id="main" className="main flex flex-column justify-content-between">
+        <div className="inner flex flex-column justify-content-between">
+          <Routes>
+            <Route
+              exact
+              path={`/`}
+              element={Home ? <Home scrollTop={scrollTop} /> : null}
+            />
+            <Route
+              path="/about"
+              element={About ? <About scrollTop={scrollTop} /> : null}
+            />
+            <Route
+              path="/contact"
+              element={Contact ? <Contact scrollTop={scrollTop} /> : null}
+            />
+            <Route
+              path="/rss"
+              element={RSSApp ? <RSSApp scrollTop={scrollTop} /> : null}
+            />
+            <Route
+              path="/youtube"
+              element={
+                YoutubeChannelVideos ? (
+                  <YoutubeChannelVideos scrollTop={scrollTop} />
+                ) : null
+              }
+            />
+            <Route
+              path="*"
+              element={Home ? <Home scrollTop={scrollTop} /> : null}
+            />
+
+            {/* 
+            <Route
+              exact
+              path={`/`}
+              element={
+                <React.Suspense
+                  fallback={
+                    <div className="text-center py-4 spinner">&nbsp;</div>
+                  }>
+                  <Home scrollTop={scrollTop} />
+                </React.Suspense>
+              }
+            />
+            <Route
+              path={`/about`}
+              element={
+                <React.Suspense
+                  fallback={
+                    <div className="text-center py-4 spinner">&nbsp;</div>
+                  }>
+                  <About scrollTop={scrollTop} />
+                </React.Suspense>
+              }
+            />
+            <Route
+              path={`/contact`}
+              element={
+                <React.Suspense
+                  fallback={
+                    <div className="text-center py-4 spinner">&nbsp;</div>
+                  }>
+                  <Contact scrollTop={scrollTop} />
+                </React.Suspense>
+              }
+            />
+  
+            <Route
+              path={`/rss`}
+              element={
+                <React.Suspense
+                  fallback={
+                    <div className="text-center py-4 spinner">&nbsp; </div>
+                  }>
+                  <RSSApp scrollTop={scrollTop} />
+                </React.Suspense>
+              }
+            />
+            <Route
+              path={`/youtube`}
+              element={
+                <React.Suspense
+                  fallback={<div className="text-center py-4 spinner"></div>}>
+                  <YoutubeChannelVideos
+                    apiKey={"AIzaSyAyd7uDEf5pKNOJRyhuQI2D2xhpaUm-h8Q"}
+                    channelId={"UCiLy-biaIPs3QddeMLIo2bw"}
+                    scrollTop={scrollTop}
+                  />
+                </React.Suspense>
+              }
+            />
+            <Route
+              path="*"
+              element={
+                <React.Suspense
+                  fallback={
+                    <div className="text-center py-4 spinner">&nbsp;</div>
+                  }>
+                  <Home scrollTop={scrollTop} />
+                </React.Suspense>
+              }
+            /> */}
+          </Routes>
+        </div>
+        <Footer />
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/com/loader/loader.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./loader.css";
+
+export default function Loader() {
+  return (
+    <div className="loader-wrapper">
+      <div className="spinner"></div>
+    </div>
+  );
+}
