3c0380fc4f2cbcbfbfd5bd134497efa3eb489fab
muhammad medhat <muhammad.medhat@gmail.com>
2026-07-15 00:44:26 +0300

    Fix formatting in vite.config.js for consistency

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/portfolio/",
   plugins: [react()],
   server: {
     port: 3000, // Keeps the default CRA port
