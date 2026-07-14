c6dafe567e3e76fa3b969ec7501943df8b199a6b
muhammad medhat <muhammad.medhat@gmail.com>
2026-07-15 00:36:08 +0300

    Update HashRouter to include future configuration options

--- src/index.jsx
+++ src/index.jsx
@@ -14,7 +14,11 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <HelmetProvider>
-      <HashRouter>
+      <HashRouter
+        future={{
+          v7_startTransition: true,
+          v7_relativeSplatPath: true,
+        }}>
         <App />
       </HashRouter>
     </HelmetProvider>
