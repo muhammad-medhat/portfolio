75d328746d8f2f5f96c97930a88da63cbc8dbfda
muhammad medhat <muhammad.medhat@gmail.com>
2026-07-15 16:22:11 +0300

    clearing git cache

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/portfolio/",
+  base: "/portfolio",
   plugins: [react()],
   server: {
     port: 3000, // Keeps the default CRA port
